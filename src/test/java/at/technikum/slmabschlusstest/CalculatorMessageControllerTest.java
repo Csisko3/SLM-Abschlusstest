819c9395683c1891a51574f6c0f08bd7f8f3dc86
csisko <w22b003@technikum-wien.at>
2023-06-21 21:17:10 +0200

    Bugfixing

--- src/test/java/at/technikum/slmabschlusstest/CalculatorMessageControllerTest.java
+++ src/test/java/at/technikum/slmabschlusstest/CalculatorMessageControllerTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorMessageControllerTest {
-
+    @Test
     void getSumTest(){
         int i = 1234;
         assertEquals(10,CalculatorMessageController.getSum(i));
